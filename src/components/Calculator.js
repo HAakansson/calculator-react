766c9f57af40533a9521c4ea2fa565f4301999ea
Niklas Håkansson <n.hakansson@hotmail.com>
2021-02-16 14:21:05 +0100

    Created a display component.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,12 @@
+import Display from "../components/Display";
 // import style from "../css/Calculator.module.css";
 import { calculator } from "../css/Calculator.module.css";
 
 function Calculator () {
   return (
     <div className={calculator}>
-      <h1>This is the calculator component</h1>
+      <Display />
+      <h1>This is the ButtonsContainer component</h1>
     </div>
   )
 }
