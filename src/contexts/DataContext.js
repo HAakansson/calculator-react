6ff7080f4108fa125e9de837afa8b6c746192f0e
Niklas Håkansson <n.hakansson@hotmail.com>
2021-02-16 15:34:53 +0100

    Created a context file, not ready yet.

---
+++ src/contexts/DataContext.js
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+
+export const DataContext = createContext();
+
+function DataProvider (props) {
+  const [dataInput, setDataInput] = useState("");
+  
+  const handleClick = (value) => {
+    console.log("Button pressed: ", value);
+  }
+
+  const values = {
+    handleClick,
+  };
+
+  return(
+    <DataContext.Provider value={values}>
+      {props.children}
+    </DataContext.Provider>
+  )
+}
+
+export default DataProvider;
