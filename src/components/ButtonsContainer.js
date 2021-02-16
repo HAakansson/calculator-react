40097b69fb0640244c15715242f330fae1da96df
Niklas Håkansson <n.hakansson@hotmail.com>
2021-02-16 14:41:10 +0100

    Made some mapping in ButtonsContainer

--- src/components/ButtonsContainer.js
+++ src/components/ButtonsContainer.js
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import { buttons } from "../css/ButtonsContainer.module.css";
 
-function ButtonsContainer () {
+function ButtonsContainer() {
+  const [buttonValues, setButtonsValues] = useState([
+    7,
+    8,
+    9,
+    "/",
+    4,
+    5,
+    6,
+    "x",
+    1,
+    2,
+    3,
+    "-",
+    "clear",
+    0,
+    "+",
+    "=",
+  ]);
+
   return (
     <div className={buttons}>
-      <p>Here we will map buttons later.</p>
+      {buttonValues.map((value) => (
+        <p key={value}>{value}</p>
+      ))}
     </div>
-  )
+  );
 }
 
 export default ButtonsContainer;
