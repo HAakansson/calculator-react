4c0cd96139963e2a553ff6557e648cba8e4df72d
Niklas Håkansson <n.hakansson@hotmail.com>
2021-02-16 13:41:15 +0100

    Created a Calculator component.

---
+++ src/components/Calculator.js
@@ -0,0 +1,12 @@
+// import style from "../css/Calculator.module.css";
+import { calculator } from "../css/Calculator.module.css";
+
+function Calculator () {
+  return (
+    <div className={calculator}>
+      
+    </div>
+  )
+}
+
+export default Calculator;
